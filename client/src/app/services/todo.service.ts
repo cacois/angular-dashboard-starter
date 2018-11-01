661c403793167ad780ace4f27b2b8f40b870751c
C. Aaron Cois <aaron.cois@gmail.com>
2018-10-31 18:35:36 -0600

    upgrading dependencies

--- client/src/app/services/todo.service.ts
+++ client/src/app/services/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { Todo } from '../models/todo';
 
 @Injectable()
