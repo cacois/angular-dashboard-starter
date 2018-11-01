494c1017cad2206d0425d2bba319265adbeed4b9
C. Aaron Cois <aaron.cois@gmail.com>
2018-10-31 18:12:15 -0600

    updating nginx config and prod run scripts

--- start_prod.sh
+++ start_prod.sh
@@ -2,8 +2,12 @@
 # This script builds both client and server in production mode, then
 # serves both via nginx
 
+# Pull dependencies for both client and server
+(cd server && yarn)
+(cd client && yarn)
+
 # Builds both client and server in production mode
 (cd server && yarn build:prod)
 (cd client && yarn build:prod)
 # Stand up daemonized docker containers to serve it all
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.prod.yml up -d
